4472e44b0505eff34e1d9752d21f000be2cfeca2
Tayyaba <tayyaba.afzal@gmail.com>
2024-01-26 19:47:37 +0000

    question 1 added to question.js

---
+++ assets/js/question.js
@@ -0,0 +1,8 @@
+const quizQuestions = [
+    {
+        question: "Arrays in Javascript can be used to store ________. ",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
+    
+]
