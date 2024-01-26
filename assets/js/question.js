29db0abbdfadd7f892315087eb3f37873fb2f8e3
Tayyaba <tayyaba.afzal@gmail.com>
2024-01-26 19:50:30 +0000

    question 2 added to question. js along with options and correct answer

--- assets/js/question.js
+++ assets/js/question.js
@@ -4,5 +4,17 @@ const quizQuestions = [
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
     },
-    
+    {
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings","booleans", "alerts", "numbers"],
+        answer: "alerts"
+    },   {
+        question: "Arrays in Javascript can be used to store ________. ",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },   {
+        question: "Arrays in Javascript can be used to store ________. ",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: "all of the above"
+    },
 ]
