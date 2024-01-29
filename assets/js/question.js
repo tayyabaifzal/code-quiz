99df67c61233e165d46e3fb816bd7cda916278b8
Tayyaba <tayyaba.afzal@gmail.com>
2024-01-29 14:00:08 +0000

    new variables for quiz questions added in questions.js. First question the quiz added

--- assets/js/question.js
+++ assets/js/question.js
@@ -3,18 +3,8 @@ const quizQuestions = [
         question: "Arrays in Javascript can be used to store ________. ",
         choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
         answer: "all of the above"
+
     },
-    {
-        question: "Commonly used data types DO NOT include:",
-        choices: ["strings","booleans", "alerts", "numbers"],
-        answer: "alerts"
-    },   {
-        question: "The condition ",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },   {
-        question: "Arrays in Javascript can be used to store ________. ",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
-    },
-]commit?????????
+
+    
+]
