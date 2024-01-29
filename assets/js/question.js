1e680d71f2ddf3901eef803976bc622a07b3b4a2
Tayyaba <tayyaba.afzal@gmail.com>
2024-01-29 15:14:35 +0000

    link to the question.js has been added to the html file

--- assets/js/question.js
+++ assets/js/question.js
@@ -21,21 +21,21 @@ const quizQuestions = [
     },
 
     {
-        questions: " A useful tool used during development and debugging or printing content to the debugger is:"
+        questions: " A useful tool used during development and debugging or printing content to the debugger is:",
         choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
         answer: "for loop"
     },
 
     {
-        questions: "String values must be enclosed within _____ when being assigned to the variables"
+        questions: "String values must be enclosed within _____ when being assigned to the variables",
         choices: ["commas", "curly brackets", "quotes", "parentheses"],
         answer: "curly brackets"
     },
 
     {
-        questions: "What is the HTML tag under which one can write the javascript code?"
+        questions: "What is the HTML tag under which one can write the javascript code?",
         choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
         answer: "<scripted>"
     },
 
-]
+];
