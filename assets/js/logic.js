8f26775a1ee32780f336d35a10b5b05feba39e4f
Tayyaba <tayyaba.afzal@gmail.com>
2024-02-16 20:14:39 +0000

    variables added to the logic js refering to the elements in the html file

---
+++ assets/js/logic.js
@@ -0,0 +1,15 @@
+const startBtn = document.getElementById("start");
+const timerEl = document.getElementById("time");
+const questionsContainer = document.getElementById("questions");
+const questionsTitleEl = document.getElementById("question-title");
+const choicesContainer = document.getElementById("choices");
+const finalScoreEl = document.getElementById("final-score");
+const initials = document.getElementById("initials");
+const submitBtn = document.getElementById("submit");
+const startScreen = document.getElementById("start-screen");
+const endScreen = document.getElementById("end-screen");
+const feedbackEl = document.getElementById("feedback");
+const correctSound = new Audio("./assets/sfx/correct.wav");
+const incorrectSound = new Audio("./assets/sfx/incorrect.wav");
+
+
--- assets/js/question.js
+++ assets/js/question.js
@@ -1,41 +1,41 @@
-const quizQuestions = [
+const questions = [
     {
         question: "Arrays in Javascript can be used to store ________. ",
-        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
-        answer: "all of the above"
+        options: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        correctAnswer: "all of the above",
 
     },
 
     {
         question: "Commonly used data types DO NOT include:",
-        choices: ["strings","booleans", "alerts", "numbers"],
-        answer: "alerts"
+        options: ["strings", "booleans", "alerts", "numbers"],
+        correctAnswer: "alerts",
 
     },
 
     {
         question: "The condition in an if/else statement is enclosed within ____.",
-        choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "all of the above"
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        correctAnswer: "all of the above",
 
     },
 
     {
-        questions: " A useful tool used during development and debugging or printing content to the debugger is:",
-        choices: ["JavaScript", "terminal/bash", "for loops", "console log"],
-        answer: "for loop"
+        question: " A useful tool used during development and debugging or printing content to the debugger is:",
+        options: ["JavaScript", "terminal/bash", "for loops", "console log"],
+        correctAnswer: "for loop",
     },
 
     {
-        questions: "String values must be enclosed within _____ when being assigned to the variables",
-        choices: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "curly brackets"
+        question: "String values must be enclosed within _____ when being assigned to the variables",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        correctAnswer: "curly brackets",
     },
 
     {
-        questions: "What is the HTML tag under which one can write the javascript code?",
-        choices: ["<javascript>", "<scripted>", "<script>", "<js>"],
-        answer: "<scripted>"
+        question: "What is the HTML tag under which one can write the javascript code?",
+        options: ["<javascript>", "<scripted>", "<script>", "<js>"],
+        correctAnswer: "<scripted>",
     },
 
 ];
